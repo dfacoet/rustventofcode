a82f956814bef3a75c851512b85106384379dd12
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-04-06 16:26:27 +0100

    2018-21-p1

--- src/bin/2018_21.rs
+++ src/bin/2018_21.rs
@@ -20,9 +20,52 @@ fn main() {
 }
 
 fn part1((ip, instructions): &(usize, Vec<Instr>)) -> usize {
-    0
+    let mut registers = [0; 6];
+
+    // r0 doesn't actually enter the computation, but only
+    // the exit condition r5 == r0 implemented by lines 28-29.
+    // If we get there, r5 is the answer.
+
+    // lines 0-5 are a test and are executed once
+    // lines 6-7 initialise the outer loop
+    
+    // lines 17-25 are a loop essentially equivalent to r1 = r3 / 256
+    // this is enough optimisation for part1
+
+    loop {
+        match registers[*ip] {
+            28 => return registers[5],
+            17 => {
+                registers[1] = registers[3] / 256;
+                registers[2] = 26;  // pointer
+            }
+            i if i > 30 => panic!("Program terminated unexpectedly"),
+            _ => {
+                // execute instruction normally
+                instructions[registers[*ip]].apply(&mut registers);
+                registers[*ip] += 1;
+            }
+        }
+    }
 }
 
 fn part2((ip, instructions): &(usize, Vec<Instr>)) -> usize {
-    0
+    let mut registers = [0; 6];
+    let mut c = 0;
+    loop {
+        match registers[*ip] {
+            28 => {println!("{}", registers[5]); registers[2] = 6;},
+            17 => {
+                registers[1] = registers[3] / 256;
+                registers[2] = 26;  // pointer
+            }
+            i if i > 30 => panic!("Program terminated unexpectedly"),
+            _ => {
+                // execute instruction normally
+                instructions[registers[*ip]].apply(&mut registers);
+                registers[*ip] += 1;
+                c += 1;
+            }
+        }
+    }
 }
