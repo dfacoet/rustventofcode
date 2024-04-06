535c1f05d8f2472feb54a615918045882a91be4f
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-04-06 17:00:23 +0100

    2018-21

--- src/bin/2018_21.rs
+++ src/bin/2018_21.rs
@@ -1,5 +1,5 @@
 use rustventofcode::device18::{parse_input, Instr};
-use std::fs;
+use std::{collections::HashMap, fs};
 
 const YEAR: u16 = 2018;
 const DAY: u8 = 21;
@@ -28,7 +28,7 @@ fn part1((ip, instructions): &(usize, Vec<Instr>)) -> usize {
 
     // lines 0-5 are a test and are executed once
     // lines 6-7 initialise the outer loop
-    
+
     // lines 17-25 are a loop essentially equivalent to r1 = r3 / 256
     // this is enough optimisation for part1
 
@@ -37,7 +37,7 @@ fn part1((ip, instructions): &(usize, Vec<Instr>)) -> usize {
             28 => return registers[5],
             17 => {
                 registers[1] = registers[3] / 256;
-                registers[2] = 26;  // pointer
+                registers[2] = 26; // pointer
             }
             i if i > 30 => panic!("Program terminated unexpectedly"),
             _ => {
@@ -50,22 +50,41 @@ fn part1((ip, instructions): &(usize, Vec<Instr>)) -> usize {
 }
 
 fn part2((ip, instructions): &(usize, Vec<Instr>)) -> usize {
+    let mut solutions = HashMap::new();
     let mut registers = [0; 6];
     let mut c = 0;
     loop {
         match registers[*ip] {
-            28 => {println!("{}", registers[5]); registers[2] = 6;},
+            28 => {
+                // Store the solutions values of r0 for which the program halts
+                // as keys; values are the number of instructions executed the first
+                // time a solution is encountered.
+                // TO THINK: when do we know we found all solutions?
+                // empirically the correct solution is found between 10^9 and 10^10 instructions
+                let sol = registers[5];
+                c += 2;
+                solutions.entry(sol).or_insert(c);
+                registers[2] = 6;
+            }
             17 => {
                 registers[1] = registers[3] / 256;
-                registers[2] = 26;  // pointer
+                registers[2] = 26; // pointer
+                c += 5 + 7 * registers[1];
             }
             i if i > 30 => panic!("Program terminated unexpectedly"),
             _ => {
                 // execute instruction normally
                 instructions[registers[*ip]].apply(&mut registers);
                 registers[*ip] += 1;
-                c += 1;
             }
         }
+        c += 1;
+    if c > 10usize.pow(10) {
+        break;
     }
+    }
+    *solutions.iter().max_by_key(|(_, &v)| v).unwrap().0
 }
+
+// 14415175 is too high
+// 9547924
