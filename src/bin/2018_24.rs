4cca4818f3505f2e0ca12a769edf872d9d28f7a8
dfacoet <11651258+dfacoet@users.noreply.github.com>
2024-10-24 09:08:12 +0100

    WIP: 2024/18 from scratch
    TODO: Figure out better structures to
    - fix borrows within round
    - reduce looping over armies?

--- src/bin/2018_24.rs
+++ src/bin/2018_24.rs
@@ -1,5 +1,13 @@
-use std::{collections::HashSet, fs, str::FromStr};
+use std::{
+    cmp::Reverse,
+    collections::{HashMap, HashSet},
+    fs,
+    iter::repeat,
+    str::FromStr,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
+use itertools::{Itertools, WhileSome};
 use regex::Regex;
 
 const YEAR: u16 = 2018;
@@ -10,104 +18,152 @@ fn main() {
     let input = fs::read_to_string(input_file).expect("Input file not found");
     let parsed_input = parse_input(input);
 
-    let sol1 = part1(&parsed_input);
-    let sol2 = part2(&parsed_input);
-
     println!("{YEAR} day {DAY}");
     println!("================");
+
+    let sol1 = part1(&parsed_input);
     println!("Part 1: {sol1}");
+
+    let sol2 = part2(&parsed_input);
     println!("Part 2: {sol2}");
 }
 
 fn parse_input(input: String) -> (Army, Army) {
-    let mut immune_system_groups = vec![];
     let mut lines = input.lines();
-    lines.next();
-    while let Some(line) = lines.next() {
-        if line.is_empty() {
-            break;
-        }
-        immune_system_groups.push(Group::from_str(line).unwrap());
+    match lines.next() {
+        Some("Immune System:") => (),
+        _ => panic!(""),
     }
-    lines.next();
-    let infection_groups = lines.map(|line| Group::from_str(line).unwrap()).collect();
+    let immune_system = lines
+        .by_ref()
+        .take_while(|line| !line.is_empty())
+        .map(|line| Group::from_str(line).unwrap())
+        .collect();
 
-    (
-        Army {
-            groups: immune_system_groups,
-        },
-        Army {
-            groups: infection_groups,
-        },
-    )
+    match lines.next() {
+        Some("Infection:") => (),
+        _ => panic!(""),
+    }
+    let infection = lines.map(|line| Group::from_str(line).unwrap()).collect();
+
+    (immune_system, infection)
 }
 
-fn part1((immune_system, infection): &(Army, Army)) -> usize {
+fn part1((immune_system, infection): &(Army, Army)) -> String {
     let mut immune_system = immune_system.clone();
     let mut infection = infection.clone();
-    immune_system.sort_by_power();
-    infection.sort_by_power();
-    while immune_system.total_units() * infection.total_units() > 0 {
-        // target selection
-        break;
+
+    while !immune_system.is_empty() && !infection.is_empty() {
+        let target_map = select_targets(&immune_system, &infection)
+            .into_iter()
+            .chain(select_targets(&infection, &immune_system))
+            .collect::<HashMap<_, _>>();
+
+        let attack_order = immune_system
+            .iter()
+            .zip(repeat(ArmyType::ImmuneSystem))
+            .chain(infection.iter().zip(repeat(ArmyType::Infection)))
+            .sorted_by_key(|(group, _)| Reverse(group.initiative));
+
+        let mut to_remove = HashSet::new();
+        for (attacker, side) in attack_order {
+            // TODO: make side a field of Group
+            if attacker.units == 0 {
+                continue;
+            }
+            if let Some(target_id) = target_map.get(&attacker.id).expect("Attacker ID not found") {
+                let target = match side {
+                    ArmyType::ImmuneSystem => infection.iter_mut().find(|g| g.id == *target_id),
+                    ArmyType::Infection => immune_system.iter_mut().find(|g| g.id == *target_id),
+                }
+                .expect("Target ID not found");
+                let units_lost = damage(attacker, target) / target.hp;
+                target.units = if units_lost < target.units {
+                    target.units - units_lost
+                } else {
+                    to_remove.insert(target.id);
+                    0
+                }
+            };
+        }
+
+        immune_system.retain(|g| !to_remove.contains(&g.id));
+        infection.retain(|g| !to_remove.contains(&g.id));
     }
-    0
+    for group in immune_system {
+        println!("{:?}", group)
+    }
+    for group in infection {
+        println!("{:?}", group)
+    }
+    "".to_string()
 }
 
-fn part2((immune_system, infection): &(Army, Army)) -> usize {
-    0
+fn part2((immune_system, infection): &(Army, Army)) -> String {
+    "".to_string()
 }
+type AttackType = String; // Could be enum, but types are not specified outside input.
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, Debug)]
 struct Group {
-    units: usize,
-    hp: usize,
-    damage: usize,
-    attack_type: String,
-    initiative: usize,
-    weaknesses: HashSet<String>,
-    immunities: HashSet<String>,
+    id: usize,
+    units: u64,
+    hp: u64,
+    attack: u64,
+    attack_type: AttackType,
+    initiative: u64,
+    weaknesses: HashSet<AttackType>,
+    immunities: HashSet<AttackType>,
 }
+
+impl Group {
+    fn effective_power(&self) -> u64 {
+        self.attack * self.units
+    }
+}
+
+#[derive(Debug)]
+struct ParseGroupError;
+static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
 
 impl FromStr for Group {
-    type Err = ();
+    type Err = ParseGroupError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let re = Regex::new(r"^(\d+) units each with (\d+) hit points\s*(\(.*?\))?\s*with an attack that does (\d+) (\w+) damage at initiative (\d+)$").unwrap();
-        let cap = re.captures(s).ok_or(())?;
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let units_hp_re = regex::Regex::new(r"(\d+) units each with (\d+) hit points").unwrap();
+        let attack_re =
+            regex::Regex::new(r"with an attack that does (\d+) (\w+) damage at initiative (\d+)")
+                .unwrap();
+        let weaknesses_immunities_re = regex::Regex::new(r"\((.*?)\)").unwrap();
 
-        let units = cap[1].parse::<usize>().unwrap();
-        let hp = cap[2].parse::<usize>().unwrap();
-        let damage = cap[4].parse::<usize>().unwrap();
-        let attack_type = cap[5].to_string();
-        let initiative = cap[6].parse::<usize>().unwrap();
+        let units_hp_caps = units_hp_re.captures(line).ok_or(ParseGroupError)?;
+        let attack_caps = attack_re.captures(line).ok_or(ParseGroupError)?;
+
+        let units = units_hp_caps[1].parse().map_err(|_| ParseGroupError)?;
+        let hp = units_hp_caps[2].parse().map_err(|_| ParseGroupError)?;
+        let attack = attack_caps[1].parse().map_err(|_| ParseGroupError)?;
+        let attack_type = attack_caps[2].to_string();
+        let initiative = attack_caps[3].parse().map_err(|_| ParseGroupError)?;
 
         let mut weaknesses = HashSet::new();
         let mut immunities = HashSet::new();
-        if let Some(modifier_cap) = cap.get(3) {
-            let modifier_str = modifier_cap
-                .as_str()
-                .trim_start_matches("(")
-                .trim_end_matches(")");
-            for section in modifier_str.split("; ") {
-                if section.starts_with("weak to ") {
-                    for weakness in section[8..].split(", ") {
-                        weaknesses.insert(weakness.to_string());
-                    }
-                } else if section.starts_with("immune to ") {
-                    for immunity in section[10..].split(", ") {
-                        immunities.insert(immunity.to_string());
-                    }
-                } else {
-                    panic!("Invalid section: {}", section);
+
+        if let Some(caps) = weaknesses_immunities_re.captures(line) {
+            let details = &caps[1];
+            for detail in details.split("; ") {
+                if let Some(stripped) = detail.strip_prefix("weak to ") {
+                    weaknesses.extend(stripped.split(", ").map(String::from));
+                } else if let Some(stripped) = detail.strip_prefix("immune to ") {
+                    immunities.extend(stripped.split(", ").map(String::from));
                 }
             }
         }
 
         Ok(Group {
+            id: NEXT_ID.fetch_add(1, Ordering::Relaxed),
             units,
             hp,
-            damage,
+            attack,
             attack_type,
             initiative,
             weaknesses,
@@ -116,24 +172,47 @@ impl FromStr for Group {
     }
 }
 
-impl Group {
-    fn effective_power(&self) -> usize {
-        self.units * self.damage
-    }
-}
+type Army = Vec<Group>;
 
 #[derive(Clone)]
-struct Army {
-    groups: Vec<Group>,
+enum ArmyType {
+    ImmuneSystem,
+    Infection,
 }
 
-impl Army {
-    fn total_units(&self) -> usize {
-        self.groups.iter().map(|g| g.units).sum()
-    }
+fn select_targets(attackers: &Army, defendants: &Army) -> HashMap<usize, Option<usize>> {
+    let mut attackers = attackers.clone();
+    attackers.sort_by_key(|group| (group.effective_power(), group.initiative));
+    let mut defendants = defendants.clone();
 
-    fn sort_by_power(&mut self) {
-        self.groups
-            .sort_by_key(|g| (g.effective_power(), g.initiative));
+    let mut target_map = HashMap::new();
+
+    for attacker in attackers {
+        let target = match defendants.iter().max_by_key(|target| {
+            (
+                damage(&attacker, target),
+                target.effective_power(),
+                target.initiative,
+            )
+        }) {
+            Some(target) if damage(&attacker, target) > 0 => Some(target.id),
+            _ => None,
+        };
+        if let Some(target_id) = target {
+            // target selected, remove from available targets
+            defendants.retain(|d| d.id != target_id);
+        }
+        target_map.insert(attacker.id, target);
+    }
+    target_map
+}
+
+fn damage(attacker: &Group, target: &Group) -> u64 {
+    if target.immunities.contains(&attacker.attack_type) {
+        0
+    } else if target.weaknesses.contains(&attacker.attack_type) {
+        2 * attacker.effective_power()
+    } else {
+        attacker.effective_power()
     }
 }
